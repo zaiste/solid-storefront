22f09c2b1377a8fa408f3aa2bb48f5c9d546927b
Zaiste <oh@zaiste.net>
2022-04-05 14:09:38 -0700

    configure Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import solid from "solid-start";
 import vercel from "solid-start-vercel";
+import netlify from "solid-start-netlify";
 
 export default defineConfig({
-  plugins: [solid({ adapter: vercel() })]
+  plugins: [solid({ adapter: netlify() })]
 });
